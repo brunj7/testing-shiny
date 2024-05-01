2f6d4f105ac979d9209ab6da230cfe129128a18c
Julien Brun <brunj7@users.noreply.github.com>
2024-05-01 14:56:05 -0700

    update test

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -30,7 +30,7 @@ test_that("{shinytest2} recording: consecutive-names-test", {
   app <- AppDriver$new(name = "consecutive-names-test", height = 904, width = 1332)
   app$set_inputs(name_input = "Julien")
   app$click("greeting_button_input")
-  app$expect_values()
+  app$expect_values("greeting_button_input")
   app$set_inputs(name_input = "")
   app$set_inputs(name_input = "Eliott")
   app$click("greeting_button_input")
@@ -42,4 +42,20 @@ test_that("{shinytest2} recording: upload-data-good", {
   app <- AppDriver$new(name = "upload-data-good", height = 904, width = 1332)
   app$upload_file(csv_input = "cols-and-data1.csv")
   app$expect_values(output="summary_table_output") #get the id from ui.R
+})
+
+
+test_that("{shinytest2} recording: default-three-species-test", {
+  app <- AppDriver$new(name = "default-three-species-test", height = 904, width = 1332)
+  app$expect_values(output = "scatterplot_output")
+})
+
+
+test_that("{shinytest2} recording: gentoo-test", {
+  app <- AppDriver$new(name = "gentoo-test", height = 904, width = 1332)
+  app$set_inputs(penguinSpp_scatterplot_input_open = TRUE, allow_no_input_binding_ = TRUE)
+  app$set_inputs(penguinSpp_scatterplot_input = c("Chinstrap", "Gentoo"))
+  app$set_inputs(penguinSpp_scatterplot_input = "Gentoo")
+  app$expect_values(output = "scatterplot_output")
+  app$set_inputs(penguinSpp_scatterplot_input_open = FALSE, allow_no_input_binding_ = TRUE)
 })
