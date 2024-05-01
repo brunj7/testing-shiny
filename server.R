278ba89fe004dab82b833758c07843807e4caddd
Julien Brun <brunj7@users.noreply.github.com>
2024-05-01 14:04:47 -0700

    add message about no name

--- server.R
+++ server.R
@@ -2,9 +2,23 @@ server <- function(input, output) {
   
   # Feature 1 ------------------------------------------------------------------
   
-  output$greeting_output <- renderText({
-    req(input$greeting_button_input) # req(): textOutput doesn't appear until button is first pressed
-    paste0("Hello ", isolate(input$name_input), "!") # isolate(): prevents textOutput from updating until button is pressed again
+  # observe() automatically re-executes when dependencies change (i.e. when `name_input` is updated), but does not return a result ----
+  observe({
     
-  }) 
+    # if the user does not type anything before clicking the button, return the message, "Please type a name, then click the Greet button." ----
+    if (nchar(input$name_input) == 0) {
+      output$greeting_output <- renderText({
+        "Please type a name, then click the Greet button."
+      })
+      
+      # if the user does type a name before clicking the button, return the greeting, "Hello [name]!" ----
+    } else {
+      output$greeting_output <- renderText({
+        paste0("Hello ", isolate(input$name_input), "!")
+      })
+    }
+  }) |>
+    
+    # Execute the above observer once the button is pressed ----
+  bindEvent(input$greeting_button_input)
 }
