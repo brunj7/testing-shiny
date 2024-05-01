70fa9b0a11b3bc8d2633b71153c6033ef977f768
Julien Brun <brunj7@users.noreply.github.com>
2024-05-01 15:32:21 -0700

    fix test

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -30,7 +30,7 @@ test_that("{shinytest2} recording: consecutive-names-test", {
   app <- AppDriver$new(name = "consecutive-names-test", height = 904, width = 1332)
   app$set_inputs(name_input = "Julien")
   app$click("greeting_button_input")
-  app$expect_values("greeting_output")
+  app$expect_values(output="greeting_output")
   app$set_inputs(name_input = "")
   app$set_inputs(name_input = "Eliott")
   app$click("greeting_button_input")
