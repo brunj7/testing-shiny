f777c150749238b04d78b8d7d198dd24d3725093
Julien Brun <brunj7@users.noreply.github.com>
2024-05-01 13:56:33 -0700

    add no & two names test

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -6,3 +6,21 @@ test_that("{shinytest2} recording: one-name-test", {
   app$click("greeting_button_input")
   app$expect_values()
 })
+
+
+test_that("{shinytest2} recording: two-names-test", {
+  app <- AppDriver$new(name = "two-names-test", height = 904, width = 1332)
+  app$set_inputs(name_input = "Karina")
+  app$click("greeting_button_input")
+  app$set_inputs(name_input = "")
+  app$set_inputs(name_input = "Eliott")
+  app$click("greeting_button_input")
+  app$expect_values()
+})
+
+
+test_that("{shinytest2} recording: no-name-test", {
+  app <- AppDriver$new(name = "no-name-test", height = 904, width = 1332)
+  app$click("greeting_button_input")
+  app$expect_values()
+})
